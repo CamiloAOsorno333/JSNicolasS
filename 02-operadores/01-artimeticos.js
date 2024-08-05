dfc289f75f3617e5b79931a8fd6166b45a81c32f
CamiloAOsorno333 <CamiloAOsorno333@gmail.com>
2024-08-05 10:26:28 -0500

    operdores aritmeticos

---
+++ 02-operadores/01-artimeticos.js
@@ -0,0 +1,21 @@
+let a=5;
+let b=7;
+
+// console.log(a+b, 'adición');
+// console.log(a-b, 'resta');
+// console.log(a*b, 'multiplicación');
+// console.log(a/b, 'división');
+// console.log(a%b, 'modulo');// modulo ej 5/7= 5 
+// console.log(a ** b, 'potencia');// potencia
+
+//incrementar
+
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+
+//decremetar
+console.log(--a);
+console.log(a--);
+console.log(a);
