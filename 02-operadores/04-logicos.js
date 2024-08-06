aab223dc0a81eb4d31d05e6408f0318d3e26b827
CamiloAOsorno333 <CamiloAOsorno333@gmail.com>
2024-08-05 20:45:25 -0500

    se termino el modulo de operadores

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,21 @@
+//AND, OR, NOT
+
+//AND &&
+let mayor= true;
+let suscrito = true;
+
+console.log(true && true);
+console.log(false && true);
+console.log(true && true);
+
+console.log('operador AND', mayor && suscrito);
+
+// OR ||
+console.log('operador AND', mayor || suscrito);
+
+
+// NOT !
+console.log('operador NOT', !mayor ); // Cuando solo se le permita acceso a algo
+
+let soloCatalogoInfantil= !mayor;
+console.log(soloCatalogoInfantil);
